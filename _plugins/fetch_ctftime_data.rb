ce3a993ef8d3298de3e8d9e4d019dacc60762e01
numberisnan <github@farazkaleemmalik.cyou>
2025-06-29 01:37:20 -0400

    Show recent CTFs on homepage

---
+++ _plugins/fetch_ctftime_data.rb
@@ -0,0 +1,77 @@
+require 'net/http'
+require 'json'
+require 'uri'
+require 'date'
+
+module Jekyll
+  class APIFetcher < Generator
+    safe true
+    priority :low
+
+    TEAM_ID = 280084
+    CACHE_FILE = "_data/ctftime_data.json"
+
+    def generate_results_urls()
+      curr_year = Date.today.year
+      urls = []
+      [curr_year, curr_year-1].each do |year| # Fetch from this and last year
+        urls.push("https://ctftime.org/api/v1/results/" + year.to_s + "/")
+      end
+
+      return urls
+    end
+
+    def fetch_json(url)
+        uri = URI(url)
+        puts "[API Fetcher] Fetching data from #{url}"
+
+        response = Net::HTTP.get_response(uri)
+        if response.is_a?(Net::HTTPSuccess)
+            return JSON.parse(response.body)
+        else
+            puts "[CTFTime] Failed to fetch data: #{response.code} #{response.message}"
+        end
+    rescue => e
+      puts "[CTFTime] Error: #{e.message}"
+    end
+
+    def result_has_team(result)
+      return result["scores"].filter { |score| score["team_id"] == TEAM_ID }
+    end
+
+    def generate(site)
+      relevant_results = nil
+      
+      if !File.exist?(CACHE_FILE) || Time.now - File.mtime(CACHE_FILE) > 86000 # One day minus a few minutes to not interfere with the cron job
+        relevant_results = []
+        generate_results_urls().each do |results_url|
+          results_json = fetch_json(results_url)
+
+          results_json.each do |key, value|
+            relevant_result = result_has_team(value)
+            if relevant_result.length > 0
+              result = relevant_result.first
+              result["title"] = value["title"]
+              result["event_id"] = key
+              result["time"] = value["time"]
+              relevant_results.push(result)
+            end
+          end
+        end
+        File.write(CACHE_FILE, JSON.pretty_generate(relevant_results))
+      else
+        age = Time.now - File.mtime(CACHE_FILE)
+        puts "[CTFTime] Using cached data (#{(age / 60).round} minutes old)"
+        relevant_results = JSON.parse(File.read(CACHE_FILE))
+      end
+
+      relevant_results.sort_by! { |result| result["time"] } # These are both in-place, sorting from recent to not
+      relevant_results.reverse!
+
+      # Inject into site.data
+      site.data['ctftime_data'] = relevant_results
+
+      puts "[CTFTime] Success!"
+    end
+  end
+end
--- index.md
+++ index.md
@@ -3,17 +3,40 @@ layout: default
 title: Homepage
 ---
 
-<img src="/assets/images/shoutbaby.png" class="logo">
+<div class="flavor">
+    <img src="/assets/images/shoutbaby.png" class="logo">
+    <p>
+        we:
+        <br>
+        <ul>
+            <li>conquer (with pwn tools)</li>
+            <li>disseminate (shellcode across the system)</li>
+            <li>corrupt (process memory sometimes)</li>
+            <li>mutate (protected data)</li>
+            <li>have a jolly good time</li>
+        </ul>
+    </p>
+</div>
+
+### Recent CTFs:
+
+<table border="1">
+    <tr>
+        <th>CTF</th>
+        <th>Points</th>
+        <th>Placement</th>
+    </tr>
+    {% for event in site.data.ctftime_data limit:7 %}
+        <tr>
+            <td>{{ event.title }}</td>
+            <td>{{ event.points }}</td>
+            <td>{{ event.place }}</td>
+        </tr>
+    {% endfor %}
+</table>
 
 [CTFTime](https://ctftime.org/team/280084)
 
 [Github](https://github.com/byte-babies)
 
 [Discord](https://discord.gg/DwXKnG8FNC)
-
-we:
-- conquer (with pwn tools)
-- disseminate (shellcode across the system)
-- corrupt (process memory sometimes)
-- mutate (protected data)
-- have a jolly good time
